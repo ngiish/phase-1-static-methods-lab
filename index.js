e526fef75fcaf25af3e7649fc1403155d9f5e0b4
Isaac <govegeku00@gmail.com>
2023-10-20 23:21:42 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g,"")
+  } 
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = string.split(' ');
+    let result = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+    return result.join(' ');
+  }
+  
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }      
+ }
+const formatter = Formatter.capitalize("crocodile")
+const sanitizer = Formatter.sanitize("ca$@#tching cold")
+const titleizer = Formatter.titleize("Maurice a an but of and for at by from end")
